e3d60d84e9ea0d95912646ed75b24e8910d43743
AlisaEKo <114878172+AlisaEKo@users.noreply.github.com>
2022-11-05 21:39:22 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,100 @@
+// let number = (10 > Math.PI);
+// console.log(number);
+
+// let p = Math.round(Math.PI);
+// console.log(p);
+
+// let y = 15 != 10;
+// console.log(y);
+
+// let z = 50 == 50;
+// console.log(z);
+
+// let k = -18 < 13;
+// console.log(k);
+
+// // let num = x;
+// function check(x) {
+//     if (x == 30) {
+//         return true;}
+//     else {
+//         return false;}
+// };
+// console.log(check(30));
+// console.log(check(20));
+// console.log(check(60));
+
+// let rand ;
+// rand = (Math.random()*20);
+// console.log(rand);
+
+// rand = Math.floor(Math.random()*10);
+// console.log(rand);
+
+// function getRndInreger(min, max)
+// {
+//     let ans;
+//     ans = Math.floor(Math.random() * (max - min + 1)) + min;
+//     return ans;
+// };
+// console.log(getRndInreger(3,7));
+// let rounds = 100
+
+// const alisa =["Tom","Mika","Ron"]
+
+// for (let i = 0; i <= rounds; i++) {
+//     console.log(Math.floor( Math.random() * 10 *i));
+// }
+
+// for (key in object) {
+
+// }
+// const person = {fname:"Alisa", lname:"Kotliarski", age:39};
+// let text = "";
+// for (let x in person) {
+//     console.log(text += person[x]);
+// }
+
+// const arr = [100, 2, 5, 35, 12];
+// let txt = "";
+// arr.forEach(testFunc);
+// function testFunc(valeu) {
+//     txt += valeu;
+// }
+// console.log(txt);
+
+
+
+// let index = 0;
+
+// while(index < 10)
+// {
+//     console.log(index);
+//     index++;
+// }
+
+
+// for(;;)
+// {
+//     console.log("forever");
+// }
+
+// Home Work 22 //
+
+let x = 10;
+let y = 15;
+function randAns(min, max)
+{
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+
+};
+let rand = randAns(x, y);
+console.log(rand);
+
+
+let somePhrase = "Alisa";
+
+for (let i = 0; i < somePhrase.length; i++) {
+    console.log(somePhrase[i]);
+}
+
